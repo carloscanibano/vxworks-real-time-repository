358b7bbfd1b0e1c7f60614a06da74c1aef3b91d8
juanjo698 <44554915+juanjo698@users.noreply.github.com>
2019-10-30 19:31:07 +0100

    Serial Works

--- controladorA.c
+++ controladorA.c
@@ -20,7 +20,7 @@
  *  Global Variables 
  *********************************************************/
 float speed = 0.0;
-int serial = 0;// 0 = SIMULATOR, 1 = SERIAL
+int serial = 1;// 0 = SIMULATOR, 1 = SERIAL
 int brk = 0; //0 = OFF, 1 = ON
 int gas = 0; //0 = OFF, 1 = ON
 int mix_status = 0; // 0 = OFF, 1 = ON
@@ -32,7 +32,6 @@ struct timespec cycle_time;
 struct timespec finish_time;
 struct timespec start_time;
 struct timespec time_difference;
-struct timespec TS;
 #define NS_PER_S  1000000000
 #define PERIOD_NS  500000000
 #define TIME_TO_RELOAD   30
@@ -94,54 +93,6 @@ void addTime(struct timespec end,
 }
 
 /**********************************************************
- *  Function: compTime 
- *********************************************************/
-int compTime(struct timespec t1, 
-			  struct timespec t2)
-{
-	if (t1.tv_sec == t2.tv_sec) {
-		if (t1.tv_nsec == t2.tv_nsec) {
-			return (0);
-		} else if (t1.tv_nsec > t2.tv_nsec) {
-			return (1);
-		} else if (t1.tv_nsec < t2.tv_nsec) {
-			return (-1);
-		}
-	} else if (t1.tv_sec > t2.tv_sec) {
-		return (1);
-	} else if (t1.tv_sec < t2.tv_sec) {
-		return (-1);
-	} 
-	return (0);
-}
-
-/**********************************************************
- *  Function: retraso 
- *********************************************************/
-double retraso ()
-{	
-	static int init = 0;
-    static int lastCount = 0;
-    static struct timespec initClock;
-    struct timespec actualClock, diffClock;
-    int actualCount;
-    int lapso;
-    int ret = 0;
-
-    if (0 == init) {
-		clock_gettime(CLOCK_REALTIME, &initClock); 
-    }
-	clock_gettime(CLOCK_REALTIME, &actualClock); 
-	diffTime(actualClock,initClock, &diffClock);
-    lapso = diffClock.tv_sec;
-    actualCount = lapso / TIME_BW_ERRORS;
-    if (lastCount < actualCount) {
-        ret = ERROR_DELAY_TIME * actualCount;
-    } 
-    return (ret);
-}
-
-/**********************************************************
  *  Function: task_speed
  *********************************************************/
 int task_speed()
@@ -160,7 +111,7 @@ int task_speed()
     // request speed
 	strcpy(request,"SPD: REQ\n");
 		
-	if(!serial){
+	if (!serial) {
 		simulator(request, answer);
 	} else {
 		writeSerialMod_9(request);
@@ -194,7 +145,7 @@ int task_slope()
 	// request slope
 	strcpy(request,"SLP: REQ\n");
 
-	if(!serial){
+	if (!serial) {
 		simulator(request, answer);
 	} else {
 		writeSerialMod_9(request);
@@ -222,20 +173,20 @@ int task_gas(){
     memset(answer,'\0',10);
     
 	
-	if ((gas == 0) && (speed <= 55)) {
+	if ( (gas == 0) && (speed <= 55) ) {
 		strcpy(request, "GAS: SET\n");
 		
-		if(!serial){
+		if (!serial) {
 			simulator(request, answer);
 		} else {
 			writeSerialMod_9(request);
 			readSerialMod_9(answer);
 		}	
 		gas = 1;	
-	} else if((gas == 1) && (speed > 55)){
+	} else if ( (gas == 1) && (speed > 55) ) {
 		strcpy(request, "GAS: CLR\n");
 		
-		if(!serial){
+		if (!serial) {
 			simulator(request, answer);
 		} else {
 			writeSerialMod_9(request);
@@ -262,10 +213,10 @@ int task_brake(){
 	memset(request,'\0',10);
 	memset(answer,'\0',10);
     			
-	if ((brk == 1) && (speed <= 55)) {
+	if ( (brk == 1) && (speed <= 55) ) {
 		strcpy(request, "BRK: CLR\n");
 		
-		if(!serial){
+		if (!serial) {
 			simulator(request, answer);
 		} else {
 			writeSerialMod_9(request);
@@ -273,10 +224,10 @@ int task_brake(){
 		}
 		
 		brk = 0;
-	} else if ((brk == 0) && (speed > 55)){
+	} else if ( (brk == 0) && (speed > 55) ){
 		strcpy(request, "BRK: SET\n");
 		
-		if(!serial){
+		if (!serial) {
 			simulator(request, answer);
 		} else {
 			writeSerialMod_9(request);
@@ -308,7 +259,7 @@ int task_mix(){
    		if (mix_status == 0) {
 			strcpy(request, "MIX: SET\n");
 			
-			if(!serial){
+			if (!serial) {
 				simulator(request, answer);
 			} else {
 				writeSerialMod_9(request);
@@ -319,7 +270,7 @@ int task_mix(){
     	} else {
     	    strcpy(request, "MIX: CLR\n");
     	    
-			if(!serial){
+			if (!serial) {
 				simulator(request, answer);
 			} else {
 				writeSerialMod_9(request);
@@ -347,24 +298,24 @@ void *controller(void *arg)
 	clock_gettime(CLOCK_REALTIME, &start_time);
 	// Endless loop
     while(1) {
-    	if(cycle == 0){
+    	if (cycle == 0) {
     		task_speed();
     		task_slope();
     		task_mix();  	
-    	}else if(cycle == 1){
+    	}else if (cycle == 1) {
     		task_gas();
     		task_brake();
-    	}else if(cycle == 2){
+    	}else if (cycle == 2) {
     		task_speed();
     		task_slope();
-    	}else if(cycle == 3){
+    	}else if (cycle == 3 ){
     		task_gas();
     		task_brake();
     		task_mix();
-    	}else if(cycle == 4){
+    	}else if (cycle == 4) {
     		task_speed();
     		task_slope();
-    	}else if(cycle == 5){
+    	}else if (cycle == 5) {
     		task_gas();
     		task_brake();
     	}
@@ -400,8 +351,8 @@ int main ()
     // init display
 	displayInit(SIGRTMAX);
 	
-	// initSerialMod_9600 uncomment to work with serial module
-	//initSerialMod_WIN_9600 ();
+	//sinitSerialMod_9600 uncomment to work with serial module
+	initSerialMod_WIN_9600 ();
 
     /* Create first thread */
     pthread_create (&thread_ctrl, NULL, controller, NULL);
